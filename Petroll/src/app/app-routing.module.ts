2707a263d184f71883e75d02929f31567140c88f
Alexander.Sol <47799833+AlexUwU@users.noreply.github.com>
2021-02-19 14:13:05 -0600

    cambios

--- Petroll/src/app/app-routing.module.ts
+++ Petroll/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
-import { NgModule } from '@angular/core';
+import { LoginComponent } from './componentes/login/login.component';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: 'login', component: LoginComponent}
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
