36222fe577024597b7bcb730ab04dda085ba98a3
Alexander.Sol <47799833+AlexUwU@users.noreply.github.com>
2021-02-19 17:47:00 -0600

    add icon

--- Petroll/src/app/app-routing.module.ts
+++ Petroll/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { LoginComponent } from './componentes/login/login.component';
+import { HomeComponent } from './componentes/home/home.component';
 import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
 
-  {path: 'login', component: LoginComponent}
+  {path: 'login', component: LoginComponent},
+  {path: 'home', component: HomeComponent},
+  {path: '', component: LoginComponent}
 ];
 
 
