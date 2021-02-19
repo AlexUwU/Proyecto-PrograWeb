bec6dbe554f0390bf39bd37df97131fb5837b809
bkevin1052 <31713984+bkevin1052@users.noreply.github.com>
2021-02-19 14:15:57 -0600

    subiendo

--- Petroll/src/app/app.module.ts
+++ Petroll/src/app/app.module.ts
@@ -3,13 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ComponentesComponent } from './componentes.component';
+import { LoginComponent } from './componentes/login/login.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-      ComponentesComponent
+    LoginComponent
    ],
   imports: [
     BrowserModule,
