a9dc05ce77e0cf9e1de572336d2dfb5c146144e4
bkevin1052 <47799833+AlexUwU@users.noreply.github.com>
2021-05-05 16:57:52 -0600

    agregando correcciones htttp

--- Petroll/src/app/backend.service.ts
+++ Petroll/src/app/backend.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,19 +11,25 @@ export class BackendService {
   constructor(private http: HttpClient) { }
   loginUsuario(data: any)
   {
-    return this.http.post<any>('EC2Co-EcsEl-E8N091GW896C-825793951.us-east-1.elb.amazonaws.com:3000/login', data, {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json'
-      })
-    });
+    return this.http.post('EC2Co-EcsEl-E8N091GW896C-825793951.us-east-1.elb.amazonaws.com:3000/petroll/login', data).pipe(catchError(this.clientError));
   }
   registerUsuario(data: any)
   {
-      return this.http.post<any>('EC2Co-EcsEl-E8N091GW896C-825793951.us-east-1.elb.amazonaws.com:3000/register', data, {
-        headers: new HttpHeaders({
-        'Content-Type': 'application/json'
-      })
-    });
+      return this.http.post('EC2Co-EcsEl-E8N091GW896C-825793951.us-east-1.elb.amazonaws.com:3000/petroll/register', data).pipe(catchError(this.clientError));
   }
+
+  clientError(error: HttpErrorResponse) {
+    let errorMessage = 'Unknown error!';
+    if (error.error instanceof ErrorEvent) {
+      // Client-side errors
+      errorMessage = `Error: ${error.error.message}`;
+    } else {
+      // Server-side errors
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+
+    return throwError(errorMessage);
+  }
+
 }
 
